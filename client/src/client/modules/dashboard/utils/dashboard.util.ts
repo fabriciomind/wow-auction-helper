77bb7538d713ac2537e00d168e21efd2ece71f61
Ukon1990 <mail@jonaskf.net>
2020-01-24 09:30:21 +0100

    .

--- client/src/client/modules/dashboard/utils/dashboard.util.ts
+++ client/src/client/modules/dashboard/utils/dashboard.util.ts
@@ -1,10 +1,44 @@
+import {Dashboard} from '../models/dashboard.model';
+import {BehaviorSubject} from 'rxjs';
+
 export class DashboardUtil {
-  setLoading(): void {
+  static list: BehaviorSubject<Dashboard[]> = new BehaviorSubject([]);
+
+  static setLoading(): void {
+    this.clearList();
+    this.forLoop(10, () => {
+      const db = new Dashboard('Loading', '');
+      db.columns = [
+        {key: 'name', title: '', dataType: ''},
+        {key: 'name', title: '', dataType: ''},
+        {key: 'name', title: '', dataType: ''}
+      ];
+      db.idParam = 'id';
+      this.forLoop(5, () => db.data.push({id: 25, name: 'Loading…'}));
+      this.addToList(db);
+    });
   }
 
-  setCalculateBoards(): void {
+  static setBoards(): void {
   }
 
-  calculateBoard(input): void {
+  static reCalculateBoards(): void {
+  }
+
+  static calculateBoard(input): void {
+  }
+
+  private static forLoop(length: number, callback: Function) {
+    for (let i = 0; i < length; ++i) {
+      callback();
+    }
+  }
+
+  private static addToList(element: any): void {
+    this.addToList([...this.list.value, element]);
+  }
+
+  private static clearList(): void {
+    this.list.next([]);
   }
 }
